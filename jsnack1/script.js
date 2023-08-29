8fde30b780c666bd386c29fcb4d028cb991cac38
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-08-29 14:04:59 +0100

    add snack1

---
+++ jsnack1/script.js
@@ -0,0 +1,28 @@
+/* Snack 1:
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+// Creare due const in successione, con prompt numerici.
+
+const num1 = Number (prompt(`Inserisci il primo numero`));
+const num2 = Number (prompt(`Inserisci il secondo numero`));
+console.log(num1, num2);
+const printEl = document.querySelector('.print')
+
+// Confrontare i due numeri.
+// Stampare il numero maggiore.
+
+if (num1 > num2) {
+    console.log(`${num1}`);
+    printEl.innerHTML = `Il primo numero e' maggiore (${num1} vs ${num2})`; 
+
+} else if (num2 > num1) {
+    console.log(`${num2}`);
+    printEl.innerHTML = `Il secondo numero e' maggiore (${num2} vs ${num1})`; 
+
+} else {
+    console.log(`I numeri sono uguali`);
+    printEl.innerHTML = `I numeri sono uguali`; 
+
+}
+
