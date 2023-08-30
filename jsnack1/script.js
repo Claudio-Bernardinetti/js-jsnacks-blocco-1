2b40190e9a18a17d4cafece62c1d2f1f3c390558
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-08-30 17:07:10 +0100

    add snack 1 while loop solution

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -34,38 +34,30 @@ Snack 1
 L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-
-
 const num1 = Number (prompt(`Inserisci il primo numero`));
 const num2 = Number (prompt(`Inserisci il secondo numero`));
 console.log(num1, num2);
 const printEl = document.querySelector('.print')
 
+
 let i = 0;
-let max = 0;
-while (i > num.length) {
 
-    
-    let num = []
+while (i < 2){
 
-        printEl.innerHTML = `Il primo numero e' maggiore (${num1} vs ${num2})`;
-        i++
-
-    /* if () {
+    if (num1 > num2) {
         console.log(`${num1}`);
         printEl.innerHTML = `Il primo numero e' maggiore (${num1} vs ${num2})`; 
-        i++ */
-        
-    /*  if (num2 > num1) {
+
+    } else if (num2 > num1) {
         console.log(`${num2}`);
         printEl.innerHTML = `Il secondo numero e' maggiore (${num2} vs ${num1})`; 
-        
+
     } else {
         console.log(`I numeri sono uguali`);
         printEl.innerHTML = `I numeri sono uguali`; 
 
     } 
- */
-}
 
-console.log(`${num}`);
+    i++
+
+}
